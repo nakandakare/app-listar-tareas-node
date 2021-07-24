a1c180cd20b95db3a81caaa012bd1e73d6f7ad08
nakandakare <kevin.nakandakare@gmail.com>
2021-07-23 22:26:06 -0300

    feature/ejercicio_3

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,43 +1,49 @@
-require('colors');
+require("colors");
 
 //Es una funcion para mostrar el menu
 const mostrarMenu = () => {
-  console.clear();
-  console.log("==============================".bgWhite);
-  console.log("   Selecciones una opcion".rainbow.bold);
-  console.log("==============================\n".bgWhite);
+  return new Promise((resolve, reject) => {
+    console.clear();
+    console.log("==============================".bgWhite);
+    console.log("   Selecciones una opcion".rainbow.bold);
+    console.log("==============================\n".bgWhite);
 
-  console.log(`${'1.'.yellow} Crear tarea`);
-  console.log(`${'2.'.magenta} Listar tarea`);
-  console.log(`${'3.'.magenta} Listar tareas completadas`);
-  console.log(`${'4.'.magenta} Listar tareas pendientes`);
-  console.log(`${'5.'.cyan} Completar tarea(s)`);
-  console.log(`${'6.'.blue} Borrar tarea`);
-  console.log(`${'7.'.bgRed} ${'Salir'.bgRed}\n`);
+    console.log(`${"1.".yellow} Crear tarea`);
+    console.log(`${"2.".magenta} Listar tarea`);
+    console.log(`${"3.".magenta} Listar tareas completadas`);
+    console.log(`${"4.".magenta} Listar tareas pendientes`);
+    console.log(`${"5.".cyan} Completar tarea(s)`);
+    console.log(`${"6.".blue} Borrar tarea`);
+    console.log(`${"0.".bgRed} ${"Salir".bgRed}\n`);
 
-  const rl = require('readline').createInterface({
+    const rl = require("readline").createInterface({
       input: process.stdin,
-      output: process.stdout
-  });
+      output: process.stdout,
+    });
 
-  rl.question('Seleccione una opción: ', (option) => {
-      if(option > 7 || option < 1) {
-        console.log('opcion invalido, tiene que ser entre 1 y 7');
+    rl.question("Seleccione una opción: ", (option) => {
+      if (option > 6 || option < 0) {
+        reject("opcion invalido, tiene que ser entre 0 y 6");
       }
+      resolve(option);
       rl.close();
+    });
   });
 };
 
 //Funcion para pausar la consola
 const pausa = () => {
-    const rl = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
+  return new Promise((resolve) => {
+    const rl = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
     });
-  
-    rl.question('Presione ENTER para continuar'.bgCyan, (option) => {
-        rl.close();
+
+    rl.question("Presione ENTER para continuar".bgCyan, (_option) => {
+      rl.close();
+      resolve();
     });
+  });
 };
 
 //exportamos las funciones;
