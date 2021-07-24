e08f9c0a8c4a77c2e57ebf99ae24c3fe8ed520b5
nakandakare <kevin.nakandakare@gmail.com>
2021-07-23 22:03:11 -0300

    feature/ejercicio_2

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -14,8 +14,34 @@ const mostrarMenu = () => {
   console.log(`${'5.'.cyan} Completar tarea(s)`);
   console.log(`${'6.'.blue} Borrar tarea`);
   console.log(`${'7.'.bgRed} ${'Salir'.bgRed}\n`);
+
+  const rl = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+
+  rl.question('Seleccione una opción: ', (option) => {
+      if(option > 7 || option < 1) {
+        console.log('opcion invalido, tiene que ser entre 1 y 7');
+      }
+      rl.close();
+  });
 };
 
+//Funcion para pausar la consola
+const pausa = () => {
+    const rl = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+  
+    rl.question('Presione ENTER para continuar'.bgCyan, (option) => {
+        rl.close();
+    });
+};
+
+//exportamos las funciones;
 module.exports = {
   mostrarMenu,
+  pausa,
 };
